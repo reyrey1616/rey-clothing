d64c97e30c2354cd77358c68aef5e66944e89e1c
YOBBADREY <dabboyrey@gmail.com>
2019-08-26 15:53:34 +0800

    installed react router and added routing feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch , Link } from 'react-router-dom';
+import Error from './components/error/error.component';
+
+const HatsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+        <Link to = "/"> Home </Link>
+       <h1> Hats {props.match.params.id} </h1> </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+
+      <Switch>
+      <Route path = "/" component = {HomePage} exact />
+      <Route path = "/shop/hats" component = {HatsPage} exact />
+      <Route component = {Error} />   
+      </Switch>
+
     </div>
   );
 }
---
+++ src/components/error/error.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Error(props) {
+    console.log(props)
+    return (
+        <div>
+            <h1> ERROR </h1>
+        </div>
+    )
+}
