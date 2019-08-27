b56a6d945c1352e7c68ff5bf3042cec942406956
YOBBADREY <dabboyrey@gmail.com>
2019-08-27 09:48:25 +0800

    added collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch , Link } from 'react-router-dom';
 import Error from './components/error/error.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = (props) => {
-  console.log(props)
-  return (
-    <div>
-        <Link to = "/"> Home </Link>
-       <h1> Hats {props.match.params.id} </h1> </div>
-  )
-}
 
 function App() {
   return (
@@ -19,7 +12,7 @@ function App() {
 
       <Switch>
       <Route path = "/" component = {HomePage} exact />
-      <Route path = "/shop/hats" component = {HatsPage} exact />
+      <Route path = "/shop" component = {ShopPage} exact />
       <Route component = {Error} />   
       </Switch>
 
